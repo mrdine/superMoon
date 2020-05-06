99e51db5b57ee3fff459e7e7a8691eb817ce85ab
Daniel Gomes <daniel.gomes071@gmail.com>
2020-05-06 16:47:18 -0300

    usando cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({ origin: `https://supermoonn.herokuapp.com`}))
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
