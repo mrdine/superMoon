338183e4048d3826818884cc42e1cb22e54bbbdc
Daniel Gomes <daniel.gomes071@gmail.com>
2020-05-06 17:49:49 -0300

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,12 +5,8 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-app.use(cors({ origin: `https://supermoonn.herokuapp.com`}))
+
+app.use(cors({ origin: `*`}))
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(routes)
