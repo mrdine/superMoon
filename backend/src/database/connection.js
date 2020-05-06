e09fddb44d7df36d9f7e87469c78bc7dd94d5f6d
Daniel Gomes <daniel.gomes071@gmail.com>
2020-05-06 16:53:06 -0300

    mudança para development

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,7 +1,7 @@
 const knex = require('knex')
 const configuration = require('../../knexfile')
 
-const env = process.env.NODE_ENV || 'development'
+const env = 'development'
 const config = configuration[env]
 
 const connection = knex(config)
