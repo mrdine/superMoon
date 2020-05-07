9f98dd032dd2397196223daca6992aca8b557860
Daniel Gomes <daniel.gomes071@gmail.com>
2020-05-07 15:22:35 -0300

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'https://supermoonn.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Headers', 'Accept','X-Requested-With,content-type', 'https://supermoonn.herokuapp.com');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
