51e22ceb0d9e68465deebe64803bf7421e3906de
Daniel Gomes <daniel.gomes071@gmail.com>
2020-05-07 13:55:34 -0300

    mudança para postgresql

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,8 +1,11 @@
 // Update with your config settings.
 require('dotenv').config()
+const pg = require('pg')
+pg.defaults.ssl = true
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 module.exports = {
-  
+  /*
     development: {
       client: 'sqlite3',
       connection: {
@@ -13,22 +16,17 @@ module.exports = {
       },
       useNullAsDefault: true,
     },
-  
-/*
+  */
+
   development: {
     client: 'pg',
-    connection: {
-      host : 'ec2-34-195-169-25.compute-1.amazonaws.com',
-      user : 'axycklmdiahsee',
-      password : '51cc42b80236cc8c833e0f83cdc84892d6f57d6878e5242c434801e60a646cc6',
-      database : 'd2tp42hvt0bg7v'
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './src/database/migrations'
     },
     useNullAsDefault: true,
   },
-*/
+
   testing: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
