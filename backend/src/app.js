9b0a60bc3cb47be0484310ff25c434b721868a8e
Daniel Gomes <daniel.gomes071@gmail.com>
2020-05-08 13:03:32 -0300

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -28,7 +28,7 @@ app.use(function (req, res, next) {
 
 
 app.use(cors({
-    'allowedHeaders': ['authorization', 'Authorization', 'Content-Type'],
+    'allowedHeaders': ['authorization', 'access-control-allow-origin' ,'Authorization', 'Content-Type'],
     'exposedHeaders': ['sessionId'],
     'origin': 'https://supermoonn.herokuapp.com',
     'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
