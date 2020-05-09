a38d3cc3d76fa1b4293266b493eed82d6f061d9f
Daniel Gomes <daniel.gomes071@gmail.com>
2020-05-09 12:19:17 -0300

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,14 +14,21 @@ app.use(cors({
 }))
 */
 app.use(cors())
-
+/*
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "*")
   next();
 });
-
+*/
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 /*
 // Add headers
 app.use(function (req, res, next) {
