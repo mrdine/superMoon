276dc57bc42a9481d0e5923a3c1a9699f1a3ce4a
Daniel Gomes <daniel.gomes071@gmail.com>
2020-05-10 13:54:15 -0300

    .

--- frontend/src/pages/EditarPerfil/index.js
+++ frontend/src/pages/EditarPerfil/index.js
@@ -113,7 +113,7 @@ export default function EditarMeuPerfil() {
             let parts = perfilFoto.name.split('.')
             if (parts[1] === 'jpg' || parts[1] === 'png') {
                 const data = new FormData()
-                data.append('file', perfilFoto, perfilFoto.name)
+                data.append('file', perfilFoto)
                 const data2 = new altFormData()
                 data2.append('file', perfilFoto, perfilFoto.name)
                 //alterar imagem
